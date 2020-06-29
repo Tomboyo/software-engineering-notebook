b52f3b8a95566d42a07a3d4660aa3740bc7e0121
Tom Simmons <thsimmon@redhat.com>
2020-06-29 18:35:14 -0400

    Initial commit with working prototype

---
+++ faulty-service/src/main/java/com/github/tomboyo/faultyservice/FaultyserviceApplication.java
@@ -0,0 +1,13 @@
+package com.github.tomboyo.faultyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaultyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaultyserviceApplication.class, args);
+	}
+
+}
