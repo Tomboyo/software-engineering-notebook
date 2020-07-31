b34dacd47eb39bba6720a509ef917a4e4a41acb9
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2020-07-30 20:55:02 -0400

    Rename ClientApplication to just Application to reflect merger

--- src/main/java/com/github/tomboyo/faultyservice/Application.java
+++ src/main/java/com/github/tomboyo/faultyservice/Application.java
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ClientApplication {
+public class Application {
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(ClientApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
